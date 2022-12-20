38ed10c72d5e41acb956886e3cd5024d25c9d7cc
adrian <adipaul1981@yahoo.ca>
2022-12-20 11:55:54 -0500

    fix: CQDG-28 fix fields for all reports

--- src/reports/clinical-data/configCqdg.ts
+++ src/reports/clinical-data/configCqdg.ts
@@ -7,20 +7,16 @@ const participants: SheetConfig = {
         { field: 'participant_id' },
         { field: 'submitter_participant_id' },
         { field: 'familyRelationships.family_id', header: 'Family ID' },
-        // FIXME Submitter Family ID????
-        // { field: 'familyRelationships.submitter_participant_id', header: 'Submitter Participant ID' },
+        { field: 'familyRelationships.submitter_family_id', header: 'Submitter Family ID' },
         { field: 'familyRelationships.relationship_to_proband', header: 'Relationship to Proband' },
         { field: 'study.name', header: 'Study Name' },
-        // FIXME Study Code missing...
-        { field: 'study.code', header: 'Study Name' },
+        { field: 'study.study_code', header: 'Study Code' },
         { field: 'familyRelationships.family_type', header: 'Family Type' },
         { field: 'gender' },
-        // FIXME missing race
         { field: 'ethnicity' },
-        // FIXME vital_status is boolean, should be changed to string?
-        { field: 'vital_status' },
-        // FIXME affected_status is missing for now
-        { field: 'age_at_recruitment' },
+        { field: 'vital_status', header: 'Vital Status' },
+        { field: 'is_affected', header: 'Affected Status' },
+        { field: 'age_at_recruitment', header: 'Age at Recruitment (Days)' },
     ],
     sort: [
         {
@@ -33,13 +29,12 @@ const participants: SheetConfig = {
 
 const phenotypes: SheetConfig = {
     sheetName: 'Phenotypes',
-    root: null,
+    root: 'observed_phenotype_tagged',
     columns: [
         { field: 'participant_id' },
         { field: 'submitter_participant_id' },
         { field: 'familyRelationships.family_id', header: 'Family ID' },
-        // FIXME Submitter Family ID????
-        // { field: 'familyRelationships.submitter_participant_id', header: 'Submitter Participant ID' },
+        { field: 'familyRelationships.submitter_family_id', header: 'Submitter Family ID' },
         { field: 'familyRelationships.relationship_to_proband', header: 'Relationship to Proband' },
         {
             field: 'observed_phenotype_tagged.name',
@@ -67,16 +62,46 @@ const phenotypes: SheetConfig = {
             },
         },
         {
-            field: 'observed_phenotype_tagged',
+            field: 'observed_phenotype_tagged.name',
+            additionalFields: ['non_observed_phenotype_tagged.name'],
             header: 'Interpretation',
-            transform: (value, row) => (value ? 'Observed' : 'Not Observed'),
+            transform: (value: string, row) => {
+                if (value) {
+                    return value;
+                } else {
+                    if (row.non_observed_phenotype_tagged) {
+                        return 'Not Observed';
+                    }
+                }
+            },
         },
         {
             field: 'observed_phenotype_tagged.age_at_event',
             header: 'Age at Phenotype (days)',
         },
     ],
-    sort: [],
+    sort: [
+        {
+            'familyRelationships.family_id': {
+                order: 'asc',
+            },
+        },
+        {
+            participant_id: {
+                order: 'asc',
+            },
+        },
+        {
+            'observed_phenotype_tagged.age_at_event': {
+                order: 'desc',
+            },
+        },
+        {
+            'non_observed_phenotype_tagged.age_at_event': {
+                order: 'desc',
+            },
+        },
+    ],
 };
 
 const diagnoses: SheetConfig = {
@@ -86,15 +111,30 @@ const diagnoses: SheetConfig = {
         { field: 'participant_id' },
         { field: 'submitter_participant_id' },
         { field: 'familyRelationships.family_id', header: 'Family ID' },
-        // FIXME Submitter Family ID????
-        { field: 'familyRelationships.submitter_participant_id', header: 'Submitter Participant ID' },
+        { field: 'familyRelationships.submitter_family_id', header: 'Submitter Family ID' },
         { field: 'familyRelationships.relationship_to_proband', header: 'Relationship to Proband' },
         { field: 'mondo_tagged.name', header: 'Diagnosis (MONDO)' },
         { field: 'icd_tagged.name', header: 'Diagnosis (ICD)' },
-        { field: 'diagnosis_source_text', header: 'Diagnosis (Source Text)' },
-        { field: 'age_at_diagnosis', header: 'Age at Diagnosis (days)' },
+        { field: 'diagnoses.diagnosis_source_text', header: 'Diagnosis (Source Text)' },
+        { field: 'diagnoses.age_at_diagnosis', header: 'Age at Diagnosis (days)' },
     ],
-    sort: [],
+    sort: [
+        {
+            'familyRelationships.family_id': {
+                order: 'asc',
+            },
+        },
+        {
+            participant_id: {
+                order: 'asc',
+            },
+        },
+        {
+            'diagnoses.age_at_diagnosis': {
+                order: 'desc',
+            },
+        },
+    ],
 };
 
 const familyRelationship: SheetConfig = {
@@ -104,11 +144,18 @@ const familyRelationship: SheetConfig = {
         { field: 'participant_id' },
         { field: 'submitter_participant_id' },
         { field: 'familyRelationships.family_id', header: 'Family ID' },
-        // FIXME Submitter Family ID????
-        { field: 'familyRelationships.submitter_participant_id', header: 'Submitter Participant ID' },
+        { field: 'familyRelationships.submitter_family_id', header: 'Submitter Family ID' },
+        { field: 'familyRelationships.focus_participant_id', header: 'Family Member ID' },
         { field: 'familyRelationships.relationship_to_proband', header: 'Relationship to Proband' },
     ],
-    sort: [{ participant_id: 'asc' }],
+    sort: [
+        { participant_id: 'asc' },
+        {
+            'familyRelationships.family_id': {
+                order: 'asc',
+            },
+        },
+    ],
 };
 
 export const queryConfigs: QueryConfig = {
@@ -116,13 +163,7 @@ export const queryConfigs: QueryConfig = {
     alias: 'participant_centric',
 };
 
-export const sheetConfigs: SheetConfig[] = [
-    participants,
-    phenotypes,
-    diagnoses,
-    // histologicalDiagnoses,
-    familyRelationship,
-];
+export const sheetConfigs: SheetConfig[] = [participants, phenotypes, diagnoses, familyRelationship];
 
 const reportConfig: ReportConfig = { queryConfigs, sheetConfigs };
 
