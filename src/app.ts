a06cff83def0877c355f5ee94d065a10f64303ee
Aymeric Toulouse <atoulouse@caresimple.com>
2023-03-24 16:45:38 -0400

    feat: CQDG-198 test disable cors n compression

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,12 @@ export default function(keycloakConfig: KeycloakConfig): Application {
     const keycloak = new Keycloak({}, keycloakConfig);
     const app = express();
 
-    app.use(
-        cors({
-            exposedHeaders: ['Content-Length', 'Content-Type', 'Content-Disposition'],
-        }),
-    );
-    app.use(compression());
+    // app.use(
+    //     cors({
+    //         exposedHeaders: ['Content-Length', 'Content-Type', 'Content-Disposition'],
+    //     }),
+    // );
+    // app.use(compression());
     app.use(express.json({ limit: '50mb' }));
     app.use(
         keycloak.middleware({
